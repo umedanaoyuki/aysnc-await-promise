7d158594c04b169af53c3cd0dabf02e2e1058ab6
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-08-27 15:56:04 +0900

    同期処理

--- practice.js
+++ practice.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+//同期処理を行う関数
+const syncronousFunc = () => {
+    console.log("これは同期処理関数内のログです");
+    return "完了";
+}
+
+//最初に実行される
+const message = "同期処理";
+//次に実行される
+const result = syncronousFunc();
+
+//最後に実行される
+console.log(message + result);
