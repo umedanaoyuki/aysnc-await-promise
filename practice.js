07117a86511f5d2fc9f0665cc892ed2ac6f22dcd
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-08-27 16:11:30 +0900

    非同期処理を実行

--- practice.js
+++ practice.js
@@ -1,26 +1,29 @@
 //非同期処理
 
 const getInformation = () => {
+    return new Promise((resolve, reject) => {
+
     const url = 'https://api.open-meteo.com/v1/forecast?latitude=35.6895&longitude=139.6917&hourly=temperature_2m,precipitation';
 
     fetch(url).then(res => res.json())
         .then(json => {
             console.log("非同期処理　成功メッセージ");
-            return json;
+            return resolve(json);
         }).catch(error => {
             console.log("非同期処理　失敗メッセージ");
-            return error;
+            return reject(error);
     })
 
+})
 }
 
 
 /**
- * getInformationを待たずに、console.logが実行されてしまう
- * apiのレスポンスを待たずに次の処理に行ってしまう
+ * 先に　非同期処理　成功メッセージ　が出力される
+ * 非同期処理が実行されていることがわかる
  */
 
 const message = "天気予報情報";
-const info = getInformation();
-
-console.log(message + info)
+getInformation().then((data) => {
+    console.log(message+ data);
+});
