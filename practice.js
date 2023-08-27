db77cc285554d01447b6d8209fd95b94d95be6db
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-08-27 16:23:34 +0900

    aysc-awaitを使って 書き直し

--- practice.js
+++ practice.js
@@ -1,29 +1,40 @@
-//非同期処理
+//非同期処理　async awaitの使用
 
-const getInformation = () => {
-    return new Promise((resolve, reject) => {
+// const message = "天気予報情報";
+//
+// const url = 'https://api.open-meteo.com/v1/forecast?latitude=35.6895&longitude=139.6917&hourly=temperature_2m,precipitation';
+// const getInformation = () => {
+//     return new Promise((resolve, reject) => {
+//
+//         fetch(url).then(res => res.json())
+//             .then(json => {
+//                 console.log("非同期処理　成功メッセージ");
+//                 return resolve(json);
+//             }).catch(error => {
+//             console.log("非同期処理　失敗メッセージ");
+//             return reject(error);
+//         })
+//     })
+// }
+// getInformation().then((data) => {
+//     console.log(message+ data);
+// });
 
-    const url = 'https://api.open-meteo.com/v1/forecast?latitude=35.6895&longitude=139.6917&hourly=temperature_2m,precipitation';
+    const getInfoByAsyncAwait =  async () => {
+        const message = "天気予報情報";
+        const url = 'https://api.open-meteo.com/v1/forecast?latitude=35.6895&longitude=139.6917&hourly=temperature_2m,precipitation\';';
 
-    fetch(url).then(res => res.json())
-        .then(json => {
-            console.log("非同期処理　成功メッセージ");
-            return resolve(json);
+        const json =  await fetch(url).then(response => {
+            console.log("非同期処理成功時のメッセージ");
+            return response.json();
         }).catch(error => {
-            console.log("非同期処理　失敗メッセージ");
-            return reject(error);
-    })
+            console.log("非同期処理失敗時のメッセージ");
+            return error;
+        });
 
-})
-}
+        console.log(message + json);
+    }
 
 
-/**
- * 先に　非同期処理　成功メッセージ　が出力される
- * 非同期処理が実行されていることがわかる
- */
+    getInfoByAsyncAwait();
 
-const message = "天気予報情報";
-getInformation().then((data) => {
-    console.log(message+ data);
-});
